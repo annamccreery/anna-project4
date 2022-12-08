3dec5d3aaf86e35fe8e2c96c2e1d055f64cbb739
Anna Mccreery <amccreery@madisoncollege.edu>
2022-12-07 18:14:35 -0600

    modified src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boom goes the dynamite!
         </p>
         <a
           className="App-link"
